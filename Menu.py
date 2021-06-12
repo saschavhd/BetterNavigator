7135257db663e27d6f3b6463870ecf7aa6c29e31
Sascha Vanheusden <48699917+saschavhd@users.noreply.github.com>
2021-06-12 14:55:24 +0200

    Update and refactoring
    Added additional features and customization
    Refactoring of variables and name change from BetterNavigator to Menu
    Several bugfixes and possible errors caught
    
    TODO:
    - Input selector, adding buttons for 1-10 selection?
    - Adding Files as attachment to message (not in message)
    - Writing documentation
    - Testing & additional bugfixing

--- Menu.py
+++ Menu.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from typing import Union
 
 
-class BetterNavigator():
+class Menu():
     def __init__(self, bot: commands.Bot, pages: list, interactor_id: int, channel: Union[discord.TextChannel, discord.DMChannel], **kwargs):
 
         self._buttons = {
@@ -16,20 +16,35 @@ class BetterNavigator():
         }
 
         self.bot = bot
-        self.pages = pages
         self.interactor_id = interactor_id
         self.channel = channel
+        self.title = kwargs.get('title', None)
+        self.description = kwargs.get('description', None)
 
-        self.current_page_number = 1
-        self._running = False
+        self.pages = pages
+        for itr, page in enumerate(self.pages):
+            if isinstance(page, Page):
+                if self.title:
+                    self.pages[itr].title = self.title
+            elif isinstance(page, (str, list, tuple)):
+                if kwargs.get('all_embedded', False):
+                    self.pages[itr] = EmbeddedPage(page, self.description, self.title, **kwargs)
+                else:
+                    self.pages[itr] = Page(page, **kwargs)
+            else:
+                raise TypeError("Items in required attribute `pages` must all be of type Page, str, list or tuple")
+
         self.input = kwargs.get('input', False)
         self.show_page_number = kwargs.get('show_page_number', True)
         self.timeout = kwargs.get('timeout', 60)
-
+        self.show_buttons = kwargs.get('show_buttons', True)
         self.remove_reactions_after = kwargs.get('remove_reactions_after', True)
         self.remove_message_after = kwargs.get('remove_message_after', False)
 
-        self._check_message_length()
+        self.current_page_number = 1
+        self._running = False
+
+        self._check_message_lengths()
 
     @property
     def current_page(self):
@@ -44,11 +59,13 @@ class BetterNavigator():
         Checks whether payload should be processed
 
         Parameters:
+        -----------
             payload: discord.RawReactionActionEvent
                 payload to check
 
         Returns:
-            : bool
+        --------
+            check: class:`bool`
                 whether to check the payload
         '''
 
@@ -63,6 +80,7 @@ class BetterNavigator():
         Creates message and starts interactive navigation
 
         Returns:
+        --------
             message: :class:`Optional[discord.Message]`
         '''
 
@@ -70,16 +88,17 @@ class BetterNavigator():
         if isinstance(page, EmbeddedPage):
             embed = page.embed
             if self.show_page_number:
-                embed.add_footer(f"{page.page_number}/{self.total_pages}")
+                embed.set_footer(f"Page {self.current_page_number}/{self.total_pages}")
             self._message = channel.send(embed=embed)
         else:
             content = page.content
             if self.show_page_number:
-                content += f"\n{page.page_number}/{self.total_pages}"
+                content += f"\n\nPage {self.current_page_number}/{self.total_pages}"
             self._message = await channel.send(content=content)
 
-        for button in self._buttons:
-            await self._message.add_reaction(button)
+        if self.show_buttons:
+            for button in self._buttons:
+                await self._message.add_reaction(button)
 
         try:
             self._running = True
@@ -87,15 +106,20 @@ class BetterNavigator():
 
             while self._running:
 
-                tasks = [
-                    asyncio.create_task(self.bot.wait_for('raw_reaction_add', check=self._check)),
-                    asyncio.create_task(self.bot.wait_for('raw_reaction_remove', check=self._check))
-                ]
+                if self.show_buttons:
+                    tasks = [
+                        asyncio.create_task(self.bot.wait_for('raw_reaction_add', check=self._check)),
+                        asyncio.create_task(self.bot.wait_for('raw_reaction_remove', check=self._check))
+                    ]
 
-                if self.input:
+                elif self.input:
                     tasks.append(
                     asyncio.create_task(self.bot.wait_for('message', check=self.input))
                     )
+
+                else:
+                    raise ValueError("Both attributes `show_buttons` and `input` ",
+                    "are False therefore no tasks were added. This is just a normal message.")
 
                 done, pending = await asyncio.wait(
                     tasks,
@@ -149,14 +173,14 @@ class BetterNavigator():
         if self.remove_reactions_after is True:
             return await self._message.clear_reactions()
 
-    def _check_message_length(self):
+    def _check_message_lengths(self):
         for i in range(1, self.total_pages):
             if isinstance(self.current_page, EmbeddedPage):
                 self.current_page = page.embed
                 if (len(embed) > 6000 or len(embed.description) > 2048):
                     raise ValueError("Embed size and it's description may not exceed 6000 and 2048 characters respectively.")
             elif len(self.current_page) > 2000:
-                    raise ValueErorr("Message size may not exceed 2000 characters.")
+                    raise ValueError("Message size may not exceed 2000 characters.")
             self.current_page_number += 1
         self.current_page_number = 1
 
@@ -168,12 +192,12 @@ class BetterNavigator():
                 if isinstance(self.current_page, EmbeddedPage):
                     embed = self.current_page.embed
                     if self.show_page_number:
-                        embed.set_footer(text=f"{self.current_page_number}/{self.total_pages}")
+                        embed.set_footer(text=f"Page {self.current_page_number}/{self.total_pages}")
                     await self._message.edit(embed=embed, content="")
                 else:
                     content = self.current_page.content
                     if self.show_page_number:
-                        content += f"\n{self.current_page_number}/{self.total_pages}"
+                        content += f"\n\nPage {self.current_page_number}/{self.total_pages}"
                     await self._message.edit(content=content, embed=None)
         return wrapper
 
@@ -209,19 +233,20 @@ class BetterNavigator():
         else:
             self.current_page_number = page_number
 
+
 class Page():
     def __init__(self,
                 content: Union[str, Union[list, tuple]],
-                page_number: int,
                 **kwargs):
 
         # Constants
         self._list_emojis = {
-        'numbers': [':zero:', ':one:', ':two:', ':three:', ':four:', ':five:', ':six:', ':seven:', ':eight:', ':nine:']
+        'numbers': [':zero:', ':one:', ':two:', ':three:',
+                    ':four:', ':five:', ':six:', ':seven:',
+                    ':eight:', ':nine:']
         }
 
         self._raw_content = content
-        self.page_number = page_number
 
         self.engrave = kwargs.get('engrave_content', None)
         self.title = kwargs.get('title', None)
@@ -240,19 +265,17 @@ class Page():
             elif self.enumerate:
                 self.prefix = [f"{itr+1} " for itr in range(len(content))]
             else:
-                prefix = kwargs.get(prefix, '')
-                self.prefix = [kwar + (' ' * (prefix != ''))] * len(content)
+                prefix = kwargs.get('prefix', '')
+                self.prefix = [prefix + (' ' * (prefix != ''))] * len(content)
         else:
-            raise TypeError(f"Required attribute content must be of type string, list or tuple. Not {type(content)}")
+            raise TypeError("Required attribute content must be of type string, ",
+            f"list or tuple. Not {type(content)}")
 
     def __str__(self):
-        cstring = ""
         if self.enlisted:
-            cstring += ''.join([f"{self.prefix[itr]}{entry}\n" for itr, entry in enumerate(self._raw_content)])
+            return ''.join([f"{self.prefix[itr]}{entry}\n" for itr, entry in enumerate(self._raw_content)]).rstrip()
         else:
-            cstring += self._raw_content
-
-        return cstring
+            return self._raw_content
 
     def __len__(self):
         return len(self.content)
@@ -261,7 +284,7 @@ class Page():
     def content(self):
         head = ""
         if self.title:
-            head += f"__**{self.title}**__\n"
+            head += f"{self.title}\n"
         if self.description:
             head += f"*{self.description}*\n"
         if self.title or self.description:
@@ -270,19 +293,22 @@ class Page():
 
     def _get_emoji_number(self, number: int) -> str:
         '''
-        Turns integer into discord emoji
+        Turns postive integer into discord emoji
 
         Parameters:
+        -----------
             number: :class:`int`
                 Integer to convert to emoji string
 
-        Returns
+        Returns:
+        --------
             emoji_number: :class:`str`
                 (Combined) string version of the integer
         '''
 
         if number < 0:
-            raise NotImplementedError
+            raise NotImplementedError("Method _get_emoji_number does not yet ",
+            "convert negative integers.")
 
         emoji_number = ''
         for char in number:
@@ -293,35 +319,57 @@ class Page():
 class EmbeddedPage(Page):
     def __init__(self,
                 content: Union[str, Union[list, tuple]],
-                page_number: int,
                 title: str,
                 description: str,
                 **kwargs):
-        super().__init__(content,
-                        page_number,
-                        title=title,
-                        description=description,
-                        **kwargs)
+        super().__init__(content, title=title, description=description, **kwargs)
 
         self.using_fields = kwargs.get('using_fields', False)
-
         if self.using_fields:
             if not isinstance(content, (list, tuple)):
-                raise TypeError("When using_fields is set to true required attribute `content` must be of type list or tuple.")
+                raise TypeError(
+                "When optional keyword attribute `using_fields` is set to true ",
+                "required attribute `content` must be of type list or tuple.",
+                f"Not {type(content)}")
             self.enlisted = True
 
+        self.author = kwargs.get('author', None)
+        self.timestamp = kwargs.get('timestamp', None)
+
         try:
-            self.colour = kwargs['colour']
+            self.image = kwargs['image_url']
         except KeyError:
-            self.colour = kwargs.get('color', discord.Colour.default())
+            self.image = kwargs.get('image', None)
+
+        try:
+            self.thumbnail = kwargs['thumnail_url']
+        except KeyError:
+            self.thumbnail = kwargs.get('thumbnail', None)
+
+        try:
+            self.colour = kwargs['color']
+        except KeyError:
+            self.colour = kwargs.get('colour', discord.Colour.default())
 
     @property
     def embed(self):
         embed = discord.Embed(
-        title=f"__{self.title}__",
+        title=f"{self.title}",
         description=f"*{self.description}*",
         colour=self.colour
         )
+
+        if self.timestamp:
+            embed.timestamp = self.timestamp
+
+        if self.author:
+            emed.set_author(name=self.author)
+
+        if self.thumbnail:
+            embed.set_thumbnail(url=self.thumbnail)
+
+        if self.image:
+            embed.set_image(url=self.image)
 
         if self.using_fields:
             for entry in self._raw_content:
@@ -330,12 +378,3 @@ class EmbeddedPage(Page):
             embed.description += f"\n\n{str(self)}"
 
         return embed
-
-
-if __name__ == '__main__':
-    mypage = Page(
-    content=['test', 'test', 'test'], page_number=2,
-    title="Test ttitle", description="testdescriptionoverhere I'll just put something ",
-    enumerate_with_emoji=True
-    )
-    print(mypage)
